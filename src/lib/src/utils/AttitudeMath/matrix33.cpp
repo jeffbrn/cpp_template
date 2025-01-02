3ca5c60feafb872a358761580d64a66e41b79e82
jeffbrn <jeff.brn@gmail.com>
2025-01-01 17:55:14 -0700

    missing specialization

--- src/lib/src/utils/AttitudeMath/matrix33.cpp
+++ src/lib/src/utils/AttitudeMath/matrix33.cpp
@@ -24,4 +24,23 @@ Matrix33<float>& Matrix33<float>::inverse() {
 	return *this;
 }
 
+template<>
+Matrix33<double>& Matrix33<double>::inverse() {
+	auto det = determinant();
+	if (det == 0.f) throw std::invalid_argument("Cannot invert a matrix with a zero determinant");
+	auto invdet = 1.f / det;
+	Matrix33<double> adj;
+	adj(0,0) = _elem[4] * _elem[8] - _elem[5] * _elem[7];
+	adj(0,1) = _elem[2] * _elem[7] - _elem[1] * _elem[8];
+	adj(0,2) = _elem[1] * _elem[5] - _elem[2] * _elem[4];
+	adj(1,0) = _elem[5] * _elem[6] - _elem[3] * _elem[8];
+	adj(1,1) = _elem[0] * _elem[8] - _elem[2] * _elem[6];
+	adj(1,2) = _elem[2] * _elem[3] - _elem[0] * _elem[5];
+	adj(2,0) = _elem[3] * _elem[7] - _elem[4] * _elem[6];
+	adj(2,1) = _elem[1] * _elem[6] - _elem[0] * _elem[7];
+	adj(2,2) = _elem[0] * _elem[4] - _elem[1] * _elem[3];
+	*this = adj * invdet;
+	return *this;
+}
+
 }
