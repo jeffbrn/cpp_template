10a6954aee341d2350031c30ef55c30096a5c69f
jeffbrn <jeff.brn@gmail.com>
2025-01-08 18:46:35 -0700

    fix run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "USER=${USER}" > .env
-echo "USER_UID=`id -u`" >> .env
-echo "USER_GID=`id -g`" >> .env
+function set_env() {
+    if [ ! -f .env ]; then
+        echo "USER=${USER}" > .env
+        echo "USER_UID=`id -u`" >> .env
+        echo "USER_GID=`id -g`" >> .env
+    fi
+}
+
+pushd .devcontainer
+set_env
+docker-compose up -d
+docker exec -ti -u ${USER} --workdir /workspace cpp-user-dev /bin/bash
+popd
