c2e9d343a8f84a9a08d4fb8e4f6b17c738ef5ba6
jeffbrn <jeff.brn@gmail.com>
2025-02-02 20:17:56 -0700

    cross-compilation

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+SVC_NAME=cpp-x86-env
+CNT_NAME=cpp-x86-env
+DO_CLEAN=false
+
+function show_help() {
+    echo "Usage: run_docker.sh [options]"
+    echo "  -a Run the ARM container"
+    echo "  -x Run the x86 container - DEFAULT"
+    echo "  -c Cleanup all docker artifacts"
+}
+
 function set_env() {
     if [ ! -f .env ]; then
         echo "USER=${USER}" > .env
@@ -8,8 +19,38 @@ function set_env() {
     fi
 }
 
+while getopts "axch" opt; do
+    case ${opt} in
+        h)
+            show_help
+            exit 0
+            ;;
+        a)
+            SVC_NAME=cpp-arm-env
+            CNT_NAME=cpp-arm-env
+            DO_CLEAN=false
+            ;;
+        x)
+            SVC_NAME=cpp-x86-env
+            CNT_NAME=cpp-x86-env
+            DO_CLEAN=false
+            ;;
+        c)
+            DO_CLEAN=true
+            ;;
+        \?)
+            echo "Invalid option: ${OPTARG}"
+            show_help
+            ;;
+    esac
+done
+
 pushd .devcontainer
-set_env
-docker-compose up -d
-docker exec -ti -u ${USER} --workdir /workspace cpp-user-dev /bin/bash
+if [ ${DO_CLEAN} = true ]; then
+    docker-compose down --rmi all --remove-orphans
+else
+    set_env
+    docker-compose up -d ${SVC_NAME}
+    docker exec -ti -u ${USER} --workdir /workspace ${CNT_NAME} /bin/bash
+fi
 popd
